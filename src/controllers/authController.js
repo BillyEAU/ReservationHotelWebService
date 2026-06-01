019d2a8bf867830921b072af4169584b9240eefb
BillyEAU <billyaron2005@gmail.com>
2026-06-01 15:42:50 +0700

    kedua

---
+++ src/controllers/authController.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from "../database/dbConfig.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+const authController = {
+    async login(req, res) {
+        const { email, password } = req.body;
+        const user = await prismaClient.findUnique({
+            where: {username: username},
+        });
+        if (!user) return res.status(404).json({ error : "Username Karyawan tidak ditemukan"});
+        const validPassword = await bcrypt.compare(password, user.password)
+        if (!validPassword) return res.status(401).json({ error : "Password Salah"});
+        const token = jwt.sign(
+            {id: Number(user.id), email: user.email, name: user.name}, process.env.JWT_SECRET, {expiresIn: "1d"},
+        );
+        })
+        })
+    }
+}
---
+++ src/database/dbConfig.js
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+// Ambil koneksi string dari env
+const connectionString = `${process.env.DATABASE_URL}`;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
+const adapter = new PrismaMariaDb(connectionString);
+
+export const prismaClient = new PrismaClient({
+  adapter: adapter,
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "event",
+      level: "error",
+    },
+    {
+      emit: "event",
+      level: "info",
+    },
+    {
+      emit: "event",
+      level: "warn",
+    },
+  ],
+});
+
+prismaClient.$on("error", (e) => {
+  console.error(e);
+});
+
+prismaClient.$on("warn", (e) => {
+  console.warn(e);
+});
+
+prismaClient.$on("info", (e) => {
+  console.info(e);
+});
+
+prismaClient.$on("query", (e) => {
+  console.info(e);
+});
