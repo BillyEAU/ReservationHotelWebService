08515612e359f860775169b3744c9d011c1cd870
radityaaryaw <91299505+radityaaryaw@users.noreply.github.com>
2026-06-09 18:36:58 +0700

    ubah index.js - radit

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 2024;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", routes);
-app.use((res, req, next) => {
+app.use((req, res, next) => {
     res.status(404).json({ error : "Endpoint tidak ada"});
 });
 // Menghubungkan routes (URL API)
